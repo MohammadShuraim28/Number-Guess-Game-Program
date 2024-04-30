0f22b285bc27eec5c2be68aa393f153a0033a80f
mohammadshuraim28 <mohammadshuraim192>
2024-04-30 16:57:41 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*10+1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "please guess the number between 1 to 10",
+
+  },
+]);
+
+if (answer.userGuess === randomNumber){
+    console.log("congract your answer is correct");
+}else {
+    console.log(" OHHH your answer is incorrect");
+}
